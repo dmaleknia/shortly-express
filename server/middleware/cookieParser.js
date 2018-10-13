189dadda3de58561d3b6a9410b5e1fe0c1b801d4
Dominic M <dominicccc@gmail.com>
2018-10-13 13:53:37 -0700

    cleaned up

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -3,33 +3,25 @@
 // which assigns req.secret so it may be used by other middleware.
 
 const parseCookies = (req, res, next) => {
-  // let username = req.body.username;
-  // let password = req.body.password;
-
-  // console.log(`req.body ${JSON.stringify(req)}`);
-
-  // console.log(req);
 
   let cookieString = req.get('Cookie') || '';
+  cookieString = cookieString.split('; ');
 
   var obj = {};
 
-  cookieString = cookieString.split('; ');
-  console.log(cookieString);
-
-  if (cookieString[0] !== '') {
+  if (cookieString[0] !== '')  {
 
     cookieString.forEach(el => {
       var sub = el.split('=');
       let key = sub[0];
       let value = sub[1];
-      console.log(`key: ${key}    value: ${value}`);
       obj[key] = value;
     });
 
   }
 
   req.cookies = obj;
+  console.log(obj);
   next();
 
 };
